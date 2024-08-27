142cc0463279b58f548c76db1475f5d67ae1e995
Blake Lundquist <blake.lundquist@hey.com>
2024-08-27 14:53:19 +0200

    clean up formatting and styles

--- src/components/BotList.tsx
+++ src/components/BotList.tsx
@@ -1,7 +1,6 @@
 import { useState } from "preact/hooks";
 import type { ResponseData } from "../utils/analyse";
 import style from "./BotList.module.css";
-import React from "preact/compat";
 
 export const BotList = ({
   bots,
@@ -10,49 +9,28 @@ export const BotList = ({
   bots: ResponseData["results"];
   icon?: string;
 }) => {
-  const [activeItemIndex, setActiveItemIndex] = useState<null | number>(null);
-
-  const toggleItem = (index: number) => {
-    if (index === activeItemIndex) {
-      setActiveItemIndex(null);
-      return;
-    }
-    setActiveItemIndex(index);
-  };
-
   return (
     <ul class="bot-list" className={style["bot-list"]}>
-      {bots.map((bot, i) => (
+      {bots.map((bot) => (
         <li>
-          <a
-            href={`/bots#${bot.name.toLowerCase()}`}
-            onClick={(e) => {
-              e.preventDefault();
-              toggleItem(i);
-            }}
-          >
-            <code>
-              {icon} {bot.name}
-            </code>
-          </a>
-          {activeItemIndex === i && (
-            <div className={style.popover}>
-              <strong>
-                <code>{bot.name}</code>
-              </strong>
-              <ul>
-                <li>
-                  <em>{bot.description}</em>
-                </li>
-                <li>
-                  Developer:{" "}
-                  <a href={bot.developer.website}>
-                    {bot.developer.name} &rarr;
-                  </a>
-                </li>
-              </ul>
-            </div>
-          )}
+          <code>
+            {icon} {bot.name}
+          </code>
+
+          <div className={style.popover}>
+            <strong>
+              <code>{bot.name}</code>
+            </strong>
+            <ul>
+              <li>
+                <em>{bot.description}</em>
+              </li>
+              <li>
+                Developer:{" "}
+                <a href={bot.developer.website}>{bot.developer.name} &rarr;</a>
+              </li>
+            </ul>
+          </div>
         </li>
       ))}
     </ul>
